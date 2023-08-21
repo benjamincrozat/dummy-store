0efe88e75d36c68f62d0b16f1996413ba2b69855
Benjamin Crozat <benjamincrozat@me.com>
2023-08-21 19:35:54 +0200

    WIP

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,15 +1,5 @@
 <html class="bg-gray-50 text-gray-600">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <script>
-        tailwind.config = {
-            theme: {
-                container: {
-                    center: true,
-                    padding: '1rem',
-                },
-            },
-        }
-    </script>
+    <title>{{ config('app.name') }}</title>
 
     <div class="container py-16">
         {{ $slot }}
