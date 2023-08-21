0d4aafe26b0579bf096f89dc908798e55cc5dfd9
Benjamin Crozat <benjamincrozat@me.com>
2023-08-21 19:42:58 +0200

    WIP

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,7 +3,11 @@
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-    <div class="container py-16">
+    <div class="container mt-16">
         {{ $slot }}
+
+        <footer class="pb-8 mt-16 text-center">
+            Demo by <a href="https://benjamincrozat.com" class="font-bold underline">Benjamin Crozat</a>.
+        </footer>
     </div>
 </html>
