c1d0fe9eebc8b8e255802a7eede610c246af3b73
Benjamin Crozat <benjamincrozat@me.com>
2023-08-21 19:37:39 +0200

    WIP

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,6 +1,8 @@
 <html class="bg-gray-50 text-gray-600">
     <title>{{ config('app.name') }}</title>
 
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
     <div class="container py-16">
         {{ $slot }}
     </div>
